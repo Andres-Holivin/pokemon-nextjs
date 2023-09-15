5afac33590f42a5b1e07ac28a3ceaab6e281e975
andres <andres12holivin@gmail.com>
2023-09-15 13:17:23 +0700

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
   const { toggleColorMode } = useColorMode();
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <CustomProviders>
           {children}
         </CustomProviders>
